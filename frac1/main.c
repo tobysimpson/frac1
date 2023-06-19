44761b7d684d1c0944eeada0cf218d868bd12fc9
Toby Simpson <toby.simpson@usi.ch>
2023-06-19 11:08:28 +0200

    Initial Commit

---
+++ frac1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  frac1
+//
+//  Created by Toby Simpson on 19.06.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
